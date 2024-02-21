9913b2f6f089c1dcde31c1eddd30cb0d82ea52e6
robertsochanski <info@jr-design.co.uk>
2024-02-21 23:42:47 +0000

    commit-1

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
-import animateTitle from './features/animateTitle'
-import createBadge from './features/createBasge'
+// import animateTitle from './features/animateTitle'
+// import createBadge from './features/createBasge'
 import './styles/style.css'
 
-console.log('Welcome to Vite + JS + Webflow!')
+// createBadge()
+// animateTitle()
 
-createBadge()
-animateTitle()
+console.log('Welcome to Vite + JS + Webflow!')
+console.log(`Hello Junior!!`)
