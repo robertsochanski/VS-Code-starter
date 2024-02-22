19ab65fdafe69580085de29a3f0b0af6f9798d96
robertsochanski <info@jr-design.co.uk>
2024-02-22 12:19:59 +0000

    test-js

---
+++ src/features/test.js
@@ -0,0 +1,7 @@
+console.log('Welcome to Vite + JS + Webflow!')
+console.log(`Hello Junior running so good!!!!`)
+console.log(`Js Delivr Test!`)
+
+const calcAge = (birthYear) => 2023 - birthYear
+const age = calcAge(1986)
+console.log(age)
